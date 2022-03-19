a426cfd2396b2acbecfa15d8b960667501dc7fea
Dh-Off <heonbella@naver.com>
2022-03-18 17:04:24 -0700

    AEB2

--- 20220317/aeb_controller.cpp
+++ 20220317/aeb_controller.cpp
@@ -7,20 +7,22 @@
  
  std_msgs::Bool flag_AEB;
  
- 
-  void UltraSonarCallback2(const std_msgs::Bool::ConstPtr& msg)
+ void UltraSonarCallback(const sensor_msgs::Range::ConstPtr& msg)
  {
-	 ROS_INFO("Sonar Range: [%f]", msg->data);
-
-	 if(msg->data)
+	 ROS_INFO("Sonar Seq: [%d]", msg->header.seq);
+	 ROS_INFO("Sonar Range: [%f]", msg->range);
+	 
+	 if(msg->range <=1.0)
 	 {
 		 ROS_INFO("AEB_Activation!!");
+		 flag_AEB.data = true;
 	 }
 	 else
 	 {
-		 ROS_INFO("AEB_Activation_Off");
+		 flag_AEB.data = false;
 	 }
  }
+ 
  
  int main(int argc, char **argv)
  {
@@ -30,12 +32,15 @@
 	 
 	 ros::NodeHandle n;
 	 
-	 ros::Rate loop_rate(1); //10
-	  
-	 ros::Subscriber sub1 = n.subscribe("/RangeSonar1", 1000, UltraSonarCallback2);
+	 ros::Rate loop_rate(10); //10
+	 
+	 ros::Publisher pub1= n.advertise<std_msgs::Bool>("/AEB",1000);
+	 	 
+	 ros::Subscriber sub = n.subscribe("range", 1000, UltraSonarCallback);
 	 
 	 while (ros::ok())
 	 {
+		 pub1.publish(flag_AEB);
 		 loop_rate.sleep();
 		 ros::spinOnce();
 		 ++count;
